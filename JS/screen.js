bd76190d4bf9c110205842931b38b29f8a71d493
GecSchool <grw7407@naver.com>
2022-08-22 10:00:28 +0900

    feat 스크린 핸들러 drag and drop
    여러가지 카드가 있을 때 drag and drop과 localstorage에 저장해
    웹 로딩 시 나오게 하기 완료

--- JS/screen.js
+++ JS/screen.js
@@ -1,6 +1,6 @@
 const dashBoard = document.querySelector(".dashBoard")
 const div = document.querySelector(".bookMark")
-let dragItem = null;
+ 
 class screenHandler{
     constructor(){
         this.HEIGHT_BLOCKS = window.screen.height / 100
@@ -30,28 +30,62 @@ class screenHandler{
 }
 
 const screen = new screenHandler()
-
-const dragDrop = (event)=>{
-    event.preventDefault();
-    console.log(1)
-    dragItem.parentNode.removeChild(dragItem)
-    event.target.appendChild(dragItem)
-    dragItem.className = 'bookMark'
-    console.log('drop')
-}
-function dragStart(){
-    console.log(this)
-    dragItem = this
-    setTimeout(() => this.className = 'hidden', 0)
-}
-const dragOver = (event)=>{
-    event.preventDefault();
-}
-const dragLeave = ()=>{
-    console.log('drag leave')
-}
-dashBoard.addEventListener('drop',dragDrop);
-div.addEventListener('dragstart',dragStart)
-dashBoard.addEventListener('dragover',dragOver)
 // dashBoard.addEventListener('dragleave',dragLeave)
-
+window.onload = (event) => {
+    let shiftX = null
+    let shiftY = null
+    let dragItem = null;
+    const Cards = document.querySelectorAll('.Card')
+    let cardCoordinates = JSON.parse(localStorage.getItem('cardCoordinates'))
+    if(!cardCoordinates){
+        cardCoordinates = {}
+        localStorage.setItem('cardCoordinates',JSON.stringify(cardCoordinates))
+    } else{
+        Object.keys(cardCoordinates).forEach(element => {
+            console.log(element)
+            
+            paintCard(Cards[element],cardCoordinates[element].X,cardCoordinates[element].Y)
+        });
+    }
+    Cards.forEach(element=>{
+        element.addEventListener('dragstart',dragStart)
+    })
+    dashBoard.addEventListener('dragover',dragOver)
+    dashBoard.addEventListener('drop',dragDrop);
+    function dragEnter(){
+        // change dashBoard background color
+    }
+    function dragLeave(){
+        // remove background color
+    }
+    function dragStart(event){
+        dragItem = this
+        shiftX = event.offsetX
+        shiftY = event.offsetY 
+        setTimeout(() => this.classList.add('hidden'), 0)
+    }
+    function paintCard(item,x,y){
+        console.log(item)
+        item.classList.add('absolute')
+        item.style.left = `${x}px`     
+        item.style.top = `${y}px`
+        item.parentNode.removeChild(item)
+        dashBoard.appendChild(item)
+    }
+    function dragDrop(event){
+        event.preventDefault();
+        const X = event.clientX - shiftX
+        const Y = event.clientY - shiftY
+        paintCard(dragItem,X,Y)
+        dragItem.classList.remove('hidden')
+        cardCoordinates[dragItem.id] = {'X': X,'Y': Y}
+        localStorage.setItem('cardCoordinates',JSON.stringify(cardCoordinates))
+        dragItem = null
+    }
+    function dragOver(event){
+        event.preventDefault();
+    }
+    
+    
+    
+};
