202337b4f9e9f58d1568ee113602b85db188631a
GecSchool <grw7407@naver.com>
2022-08-23 10:50:43 +0900

    repit : screen handler
    전체 스크린에서 div관리 수정
    sideBar에서 drag후 sideBar에 drop
    dashBoard에서 drag후 sideBar에 drop
    dashBoard에서 card에 margin존재 시 정확한 top 값 할당

--- JS/screen.js
+++ JS/screen.js
@@ -1,5 +1,3 @@
-const dashBoard = document.querySelector(".dashBoard")
-const div = document.querySelector(".bookMark")
  
 class screenHandler{
     constructor(){
@@ -30,50 +28,110 @@ class screenHandler{
 }
 
 const screen = new screenHandler()
-// dashBoard.addEventListener('dragleave',dragLeave)
 window.onload = (event) => {
+    const dashBoard = document.querySelector(".dashBoard")
+    const sideBar = document.querySelector(".sideBar")
+    const Cards = document.querySelectorAll('.Card')
+
     let shiftX = null
     let shiftY = null
     let dragItem = null;
-    const Cards = document.querySelectorAll('.Card')
+
     let cardCoordinates = JSON.parse(localStorage.getItem('cardCoordinates'))
     if(!cardCoordinates){
         cardCoordinates = {}
         localStorage.setItem('cardCoordinates',JSON.stringify(cardCoordinates))
     } else{
         Object.keys(cardCoordinates).forEach(element => {
-            console.log(element)
-            
             paintCard(Cards[element],cardCoordinates[element].X,cardCoordinates[element].Y)
         });
     }
     Cards.forEach(element=>{
         element.addEventListener('dragstart',dragStart)
+        element.style.marginTop = '40px'
     })
+
     dashBoard.addEventListener('dragover',dragOver)
-    dashBoard.addEventListener('drop',dragDrop);
-    function dragEnter(){
-        // change dashBoard background color
+    dashBoard.addEventListener('drop',dropOnDashBoard);
+    // dashBoard.addEventListener('dragenter',dragEnter)
+    // dashBoard.addEventListener('dragleave',dragLeave)
+    sideBar.addEventListener('drop',dropOnSideBar)
+    sideBar.addEventListener('dragover',dragOver)
+    console.log(2)
+    sideBar.onmouseover = ()=>{
+        sideBarHover()
     }
-    function dragLeave(){
-        // remove background color
+    sideBar.onmouseout = ()=>{
+        sideBarHover()
     }
+    // sideBar.addEventListener('dragenter',sideBarHover)
+    // sideBar.addEventListener('dragleave',sideBarHover)
+    function sideBarHover(){
+        // event.preventDefault();
+        console.log(1)
+        sideBar.classList.toggle('sideBarHover')
+    }
+    // function dragEnter(){
+    //     // change dashBoard background color
+    //     console.log(12)
+    //     // sideBar.classList.add('sideBarHover')
+    // }
+    // function dragLeave(){
+    //     // remove background color
+    //     console.log(12)
+    //     // sideBar.classList.remove('sideBarHover')
+    // }
     function dragStart(event){
         dragItem = this
-        shiftX = event.offsetX
-        shiftY = event.offsetY 
+        shiftX = event.clientX - this.getBoundingClientRect().left 
+        shiftY = event.clientY - this.getBoundingClientRect().top + parseInt(this.style.marginTop.slice(0,-2))
         setTimeout(() => this.classList.add('hidden'), 0)
     }
     function paintCard(item,x,y){
-        console.log(item)
         item.classList.add('absolute')
         item.style.left = `${x}px`     
         item.style.top = `${y}px`
         item.parentNode.removeChild(item)
         dashBoard.appendChild(item)
     }
-    function dragDrop(event){
-        event.preventDefault();
+    function dropOnSideBar(event){
+        event.preventDefault()
+        dragItem.classList.remove('absolute')
+        dragItem.style.left = null
+        dragItem.style.top = null
+        const indexSearch = function (arr,target){
+            let left = 0
+            let right = arr.length
+            let mid = null
+            target = parseInt(target)
+            while(left < right){
+                mid = left + parseInt((right - left) / 2)
+                if(arr[mid].id < target){
+                    left = mid + 1
+                } else if(arr[mid].id == target){
+                    return true
+                } else{
+                    right = mid
+                }
+            }
+            return left
+        }
+        
+        dragItem.classList.remove('hidden')
+        const sideCard = document.querySelector('.sideCard')
+        const INDEX = indexSearch(sideCard.children,dragItem.id)
+        if(INDEX !== true){
+            dashBoard.removeChild(dragItem)
+            sideCard.insertBefore(dragItem,sideCard.children[INDEX])
+            delete(cardCoordinates[dragItem.id])
+            localStorage.setItem('cardCoordinates',JSON.stringify(cardCoordinates))
+        
+        }
+        dragItem = null
+    }
+    function dropOnDashBoard(event){
+        event.preventDefault()
+        // if 겹치는 항목 있다 -> cancel
         const X = event.clientX - shiftX
         const Y = event.clientY - shiftY
         paintCard(dragItem,X,Y)
@@ -85,7 +143,5 @@ window.onload = (event) => {
     function dragOver(event){
         event.preventDefault();
     }
-    
-    
     
 };
