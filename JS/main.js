9f41b50a7d613395882836ee0e6c63507124688c
GecSchool <grw7407@naver.com>
2022-08-27 04:40:38 +0900

    refactoy: js구조 변경
    main파일을 중심으로 js파일들을 실행하는 구조로 변환함

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+window.onload = ()=>{
+    const LOCAL_COLLECTION_KEY = 'TDLS'
+    const LOCAL_RECENT_KEY = 'todayTDL'
+    screenHandler()
+    ToDoListHandler()
+}
--- JS/screen.js
+++ JS/screen.js
@@ -1,34 +1,34 @@
- 
-class screenHandler{
-    constructor(){
-        this.HEIGHT_BLOCKS = window.screen.height / 100
-        this.WIDTH_BLOCKS = window.screen.width / 100
-        this.MARGIN_LEFT = (window.screen.width % 100) / 2
-        this.MARGIN_TOP = (window.screen.height % 100) / 2
-        this.cardLocation = []
-    }
-    set setCardLocation(location){
-        this.cardLocation.add(location)
-    }
-    get getScreenWidth(){
-        return this.WIDTH_BLOCKS
-    }
-    get getScreenHeight(){
-        return this.HEIGHT_BLOCKS
-    }
-    get getMarginLeft(){
-        return this.MARGIN_LEFT
-    }
-    get getMarginTop(){
-        return this.MARGIN_TOP
-    }
-    getCardLocation(index){
-        return this.cardLocation[index]
-    }
-}
+// class screenHandler{
+//     constructor(){
+//         this.HEIGHT_BLOCKS = window.screen.height / 100
+//         this.WIDTH_BLOCKS = window.screen.width / 100
+//         this.MARGIN_LEFT = (window.screen.width % 100) / 2
+//         this.MARGIN_TOP = (window.screen.height % 100) / 2
+//         this.cardLocation = []
+//     }
+//     set setCardLocation(location){
+//         this.cardLocation.add(location)
+//     }
+//     get getScreenWidth(){
+//         return this.WIDTH_BLOCKS
+//     }
+//     get getScreenHeight(){
+//         return this.HEIGHT_BLOCKS
+//     }
+//     get getMarginLeft(){
+//         return this.MARGIN_LEFT
+//     }
+//     get getMarginTop(){
+//         return this.MARGIN_TOP
+//     }
+//     getCardLocation(index){
+//         return this.cardLocation[index]
+//     }
+// }
 
-const screen = new screenHandler()
-window.onload = (event) => {
+// const screen = new screenHandler()
+const screenHandler = () => {
+    console.log(1)
     const dashBoard = document.querySelector(".dashBoard")
     const sideBar = document.querySelector(".sideBar")
     const Cards = document.querySelectorAll('.Card')
@@ -89,7 +89,7 @@ window.onload = (event) => {
         dragItem = this
         shiftX = event.clientX - this.getBoundingClientRect().left 
         shiftY = event.clientY - this.getBoundingClientRect().top + parseInt(this.style.marginTop.slice(0,-2))
-        setTimeout(() => this.classList.add('hidden'), 0)
+        // setTimeout(() => this.classList.add('hidden'), 0)
     }
     function paintCard(item,x,y){
         item.classList.add('absolute')
